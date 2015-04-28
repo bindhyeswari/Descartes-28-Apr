d3f7540a29ea21f9e4b409190e7dc3e34e320541
mishra <tech@golivelabs.io>
2015-04-28 11:30:00 -0700

    asdf

---
+++ README.md
@@ -0,0 +1,10 @@
+#Attendees
+Nina, Raymond and Aditya
+
+#Homework
+-- Use async to read a set of images in the data uri format and print the array once all the images have been read. 
+-- What is the bitwise operator ~? What does ~~ do?
+-- Nodejs documentation - Path, fs, module, http
+
+#References
+-- https://nodejs.org/api/
---
+++ app.js
@@ -0,0 +1,33 @@
+var request = require('request');
+var fs = require('fs');
+var uuid = require('uuid');
+var path = require('path');
+var async = require('async');
+
+var imgsrcs = require('./sources');
+
+function downloadFile(url, callback) {
+
+    var ext = path.extname(url);
+    var filepathname = 'images/' + uuid.v4() + ext;
+    console.log(filepathname);
+
+    // callback is invoked after reading the image, or writing the image is complete
+    var stream_r = request(url);
+    var stream_w = fs.createWriteStream(filepathname);
+    stream_r.on('end', function () {
+        console.log('finished reading ' + url + ' from http');
+    });
+    stream_w.on('finish', function () {
+        callback(null, url);
+    });
+    stream_r.pipe(stream_w);
+}
+
+// download all the images from the yahoo page
+async.parallelLimit(imgsrcs.map(function (src) {
+    return downloadFile.bind(null, src);
+}), 4, function (err, results) {
+    if (err) console.log('something broke');
+    else console.log('done with all the images');
+});
